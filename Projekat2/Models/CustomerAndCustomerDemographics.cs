d81bddd5096c92b8217950cadc51ed963f2f7542
Igy97 <igorbrdaric@gmail.com>
2021-01-11 22:46:59 +0100

    Third task maybe it's completed, generated part created and on of your idea

---
+++ Projekat2/Models/CustomerAndCustomerDemographics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat2.Models
+{
+    public class CustomerAndCustomerDemographics
+    {
+        public Customers customers { get; set; }
+        public IEnumerable<CustomerDemographics> customerDemographics { get; set; }
+    }
+}
