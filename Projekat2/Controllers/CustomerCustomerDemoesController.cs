9824336122c056d39862a7ec2e4204cdb0f44e3a
Igy97 <igorbrdaric@gmail.com>
2021-01-16 18:21:53 +0100

    View look improved

--- Projekat2/Controllers/CustomerCustomerDemoesController.cs
+++ Projekat2/Controllers/CustomerCustomerDemoesController.cs
@@ -24,27 +24,7 @@ namespace Projekat2.Controllers
             var northwindContext = _context.CustomerCustomerDemo.Include(c => c.Customer).Include(c => c.CustomerType);
             return View(await northwindContext.ToListAsync());
         }
-
-        // GET: CustomerCustomerDemoes/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var customerCustomerDemo = await _context.CustomerCustomerDemo
-                .Include(c => c.Customer)
-                .Include(c => c.CustomerType)
-                .FirstOrDefaultAsync(m => m.CustomerId == id);
-            if (customerCustomerDemo == null)
-            {
-                return NotFound();
-            }
-
-            return View(customerCustomerDemo);
-        }
-
+ 
         // GET: CustomerCustomerDemoes/Create
         public IActionResult Create()
         {
@@ -69,92 +49,6 @@ namespace Projekat2.Controllers
             ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", customerCustomerDemo.CustomerId);
             ViewData["CustomerTypeId"] = new SelectList(_context.CustomerDemographics, "CustomerTypeId", "CustomerTypeId", customerCustomerDemo.CustomerTypeId);
             return View(customerCustomerDemo);
-        }
-
-        // GET: CustomerCustomerDemoes/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var customerCustomerDemo = await _context.CustomerCustomerDemo.FindAsync(id);
-            if (customerCustomerDemo == null)
-            {
-                return NotFound();
-            }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", customerCustomerDemo.CustomerId);
-            ViewData["CustomerTypeId"] = new SelectList(_context.CustomerDemographics, "CustomerTypeId", "CustomerTypeId", customerCustomerDemo.CustomerTypeId);
-            return View(customerCustomerDemo);
-        }
-
-        // POST: CustomerCustomerDemoes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("CustomerId,CustomerTypeId")] CustomerCustomerDemo customerCustomerDemo)
-        {
-            if (id != customerCustomerDemo.CustomerId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(customerCustomerDemo);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CustomerCustomerDemoExists(customerCustomerDemo.CustomerId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", customerCustomerDemo.CustomerId);
-            ViewData["CustomerTypeId"] = new SelectList(_context.CustomerDemographics, "CustomerTypeId", "CustomerTypeId", customerCustomerDemo.CustomerTypeId);
-            return View(customerCustomerDemo);
-        }
-
-        // GET: CustomerCustomerDemoes/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var customerCustomerDemo = await _context.CustomerCustomerDemo
-                .Include(c => c.Customer)
-                .Include(c => c.CustomerType)
-                .FirstOrDefaultAsync(m => m.CustomerId == id);
-            if (customerCustomerDemo == null)
-            {
-                return NotFound();
-            }
-
-            return View(customerCustomerDemo);
-        }
-
-        // POST: CustomerCustomerDemoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var customerCustomerDemo = await _context.CustomerCustomerDemo.FindAsync(id);
-            _context.CustomerCustomerDemo.Remove(customerCustomerDemo);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool CustomerCustomerDemoExists(string id)
